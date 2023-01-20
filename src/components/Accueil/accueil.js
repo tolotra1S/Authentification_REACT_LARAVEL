8d6acef542a27f19000821bdabf22bbb20f5ddd1
tolotra1S <tolotraranarison@gmail.com>
2023-01-20 16:09:13 +0300

    second commit

--- src/components/Accueil/accueil.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function accueil() {
-  return (
-    <div><h1>Ici c'est contact</h1></div>
-  )
-}
-
-export default accueil
--- src/components/Contact/contact.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function contact() {
-  return (
-    <div><h1>Ici c'est contact</h1></div>
-  )
-}
-
-export default contact
